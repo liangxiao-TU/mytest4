e372561133726bb443808d7316d4202321582a69
liangxiao <1973830704@qq.com>
2020-03-14 14:22:58 +0800

    121

---
+++ src/com/uzz/test/firsttest.java
@@ -0,0 +1,11 @@
+package com.uzz.test;
+
+/**
+ * @author LiangXiao
+ * @date 2020/3/14-14:03
+ */
+public class firsttest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
