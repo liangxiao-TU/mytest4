06541ab38793c6109701de93910071864e59f57b
liangxiao <1973830704@qq.com>
2020-03-14 14:45:35 +0800

    1

--- src/com/uzz/test/firsttest.java
+++ src/com/uzz/test/firsttest.java
@@ -7,6 +7,5 @@ package com.uzz.test;
 public class firsttest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-        System.out.println("Hello Git");
     }
 }
